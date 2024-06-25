1c4f6e8a6132bd4af17e8670301c609590c7577f
Nicholas Williams <nicholaswilliams910@gmail.com>
2024-06-25 14:59:25 -0500

    added dynamically rendered area location component through fetch call to local API and invoked/rendered in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,16 @@
 import { OwnJeansChoices } from "./OwnsJeans.js"
+import { LocationTypeChoices } from "./UrbanDweller.js"
 
 const container = document.querySelector("#container")
 
-const render = () => {
+const render = async () => {
 	const jeanOwnershipHTML = OwnJeansChoices()
+	const locationsHTML = await LocationTypeChoices()
 
-	container.innerHTML = jeanOwnershipHTML
+	container.innerHTML = `
+    ${jeanOwnershipHTML}
+    ${locationsHTML}
+    `
 }
 
 render()
